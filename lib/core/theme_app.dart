a33ff49cdf76db6ee1e4b0af477d9f3ee7e07143
Leandro Marini <42304213+leandromarini@users.noreply.github.com>
2020-10-26 19:56:58 -0300

    Desafio 3 - Splash Screen

--- lib/core/theme_app.dart
+++ lib/core/theme_app.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 
 final themeApp = ThemeData(
   visualDensity: VisualDensity.adaptivePlatformDensity,
-  primarySwatch: Colors.red,
+  primarySwatch: Colors.blue,
   buttonTheme: ButtonThemeData(
     height: 52,
     textTheme: ButtonTextTheme.primary,
--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:tictactoe/core/constants.dart';
 import 'package:tictactoe/core/theme_app.dart';
 import 'pages/game_page.dart';
+import 'package:custom_splash/custom_splash.dart';
 
 void main() {
   runApp(MyApp());
@@ -14,7 +15,12 @@ class MyApp extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       title: GAME_TITLE,
       theme: themeApp,
-      home: GamePage(),
+      home: CustomSplash(
+        imagePath: 'assets/images/splash_screen.png',
+        backGroundColor: Theme.of(context).accentColor,
+        duration: 3000,
+        home: GamePage(),
+      ),
     );
   }
 }
